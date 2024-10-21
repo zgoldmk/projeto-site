5b99cb89e1d3f71b36ed794e53cd4a6d992b90d5
zgoldmk <kauaandrade@hotmail.com.br>
2024-10-20 23:43:14 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const registerBtn = document.getElementById('register');
+const container = document.getElementById('container');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () =>
+{
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () =>
+{
+    container.classList.remove("active");
+});
